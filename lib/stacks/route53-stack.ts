64bbbec2693191acedd454fde1368fb6bc4d4108
joj <young2k15@tutanota.com>
2024-12-14 18:12:37 -0500

    small fix

--- lib/stacks/route53-stack.ts
+++ lib/stacks/route53-stack.ts
@@ -3,7 +3,7 @@ import { AwsStackBase, BaseStackProps } from './stackbase';
 import { Route53Zone } from '@cdktf/provider-aws/lib/route53-zone';
 
 export class Route53ZoneStack extends AwsStackBase {
-    public zone; Route53Zone;
+    public zone: Route53Zone;
     constructor(scope: Construct, id: string, props: BaseStackProps) {
         super(scope, `${props.name}-${props.project}-${id}`, {
             name: props.name,
@@ -13,6 +13,6 @@ export class Route53ZoneStack extends AwsStackBase {
 
         this.zone = new Route53Zone (this, `${id}`, {
             name: `${props.name}.${props.project}.com`,
-        }
+        });
     }
-]}
+}
